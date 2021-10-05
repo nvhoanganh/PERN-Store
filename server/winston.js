8f6f700540c3319376e65c882c64fa1b0a4466fd
anguyen1909 <nvhoanganh1909@gmail.com>
2021-10-05 21:02:47 +0000

    use winston

---
+++ server/winston.js
@@ -0,0 +1,28 @@
+const { createLogger, format, transports } = require('winston');
+const newrelicFormatter = require('@newrelic/winston-enricher');
+const { combine, timestamp, label, printf } = format;
+const appRoot = require('app-root-path');
+
+const logger = createLogger({
+	level: 'info',
+	format: format.combine(newrelicFormatter()),
+	defaultMeta: { service: 'backend-api' },
+	transports: [
+		new transports.File({
+			filename: `logs/api.log`,
+			maxFiles: 5,
+			handleExceptions: false,
+			maxsize: 5242880,
+			colorize: false,
+			json: false,
+		}),
+	],
+});
+
+logger.stream = {
+	write: function (message, encoding) {
+		logger.info(message);
+	},
+};
+
+module.exports = logger;
